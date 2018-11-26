ff719a4f51ce5b11e37deff69b8499a464725b85
codenation12 <codenation12@bitbucket.org>
2018-11-26 13:00:20 +0530

    updating code

--- pygame/first.py
+++ pygame/first.py
@@ -4,11 +4,11 @@ pygame.init()
 win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First Game")
 
-x=50
-y=50
-width=50
-height=50
-vel=5
+x=2
+y=2
+width=40
+height=60
+vel=10
 
 run = True
 while run:
@@ -20,16 +20,16 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
         x += vel
 
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and y > vel:
         y -= vel
 
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and y < 500 - height - vel:
         y += vel
 
     win.fill((0,0,0))
