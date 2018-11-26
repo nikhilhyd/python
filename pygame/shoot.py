15f00c60d0c1a85e4837b054fc4f2ab917510fe5
codenation12 <codenation12@bitbucket.org>
2018-11-26 21:13:47 +0530

    adding files

---
+++ pygame/shoot.py
@@ -0,0 +1,49 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Shoot Game")
+
+x=230
+y=460
+width=30
+height=30
+vel=10
+ox=10
+oy=10
+pygame.time.set_timer ( pygame.USEREVENT , 2 )
+
+run = True
+while run:
+    pygame.time.delay(5)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                z = y-30
+                while z > vel:
+                    z -= 2
+                    pygame.time.delay(10)
+                    win.fill((0,0,0))
+                    pygame.draw.rect(win, (255,0,0), (x,z,width,height))
+                    pygame.draw.rect(win, (255,255,0), (x,y,width,height))
+                    pygame.display.update()
+
+            while ox < 500 - width - vel:
+                ox += vel
+                pygame.time.delay(50)
+                win.fill((0,0,0))
+                pygame.draw.rect(win, (255,255,0), (ox,oy,width,height))
+                pygame.draw.rect(win, (255,255,0), (x,y,width,height))
+                pygame.display.update()
+
+            ox=10
+
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (255,255,0), (x,y,width,height))
+    pygame.display.update()
+
+pygame.quit()
